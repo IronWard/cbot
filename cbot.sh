cd5ea93fada13278acb91426e3a1e6591807e678
IronWard <IronWard@gmail.com>
2014-01-04 12:26:59 +0800

    First commit : cBot - A Bash Scrip IRC Bot

---
+++ cbot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# directory where this script is
+BOTDIR=$(dirname $0)
+# directory of core components
+COREDIR="$BOTDIR/core"
+# directory of event handler modules
+MODDIR="$BOTDIR/modules"
+# directory of plugins
+PLUGINDIR="$BOTDIR/plugins"
+# directory of transport modules
+TRANDIR="$BOTDIR/transport"
+# path to the configuration file
+CONFIG="$BOTDIR/cbot.cfg"
+# return code
+RC=0
+
+# load the cbot core
+source "$COREDIR/cbot"
+
+# setup the bot
+cbot.setup
+
+# run the bot
+cbot.run
+
+# cleanup the bot
+cbot.cleanup
+
+# exit
+exit $RC
+
